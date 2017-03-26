c8daf109c5bca1b7858625c621d339972e1b73e5
blackice999 <adam596341@yahoo.com>
2017-03-26 19:31:25 +0300

    Add pretty links

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Web shop</title>
-    <link rel="stylesheet" type="text/css" href="css/foundation.css">
+    <link rel="stylesheet" type="text/css" href="/paginaweb/css/foundation.css">
 </head>
 <body>
     <?php include "menu.php"; ?>
--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div class="row">
 
     <div class="large-10 medium-10 small-10 small-centered medium-centered large-centered columns">
-        <img class="float-center" src="img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
+        <img class="float-center" src="/paginaweb/img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
 
         <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
             <button class="menu-icon" type="button" data-toggle="example-menu"></button>
@@ -9,45 +9,45 @@
         </div>
 
         <ul class="dropdown menu" data-dropdown-menu id="example-menu">
-            <li><a href="index.php" class="active">Home</a></li>
-            <li><a href="computer_parts.html">Computer parts</a>
+            <li><a href="/paginaweb/index" class="active">Home</a></li>
+            <li><a href="/paginaweb/computer/parts">Computer parts</a>
                 <ul class="menu vertical">
                     <li><a href="motherboards.html">Motherboards</a></li>
                     <li><a href="motherboards.html">Motherboards</a></li>
                 </ul>
             </li>
-            <li><a href="peripherials.html">Peripherials</a>
+            <li><a href="/paginaweb/peripherials">Peripherials</a>
                 <ul class="menu vertical">
                     <li><a href="test.html">asdf</a></li>
                     <li><a href="test.html">asdf</a></li>
                 </ul>
             </li>
-            <li><a href="software.html">Software</a>
+            <li><a href="/paginaweb/software">Software</a>
                 <ul class="menu vertical">
                     <li><a href="test.html">asdf</a></li>
                     <li><a href="test.html">asdf</a></li>
                 </ul>
             </li>
-            <li><a href="telephones.html">Telephones</a>
+            <li><a href="/paginaweb/telephones">Telephones</a>
                 <ul class="menu vertical">
                     <li><a href="test.html">asdf</a></li>
                     <li><a href="test.html">asdf</a></li>
                 </ul>
             </li>
-            <li><a href="audio_foto">Audio Photo/video</a>
+            <li><a href="/paginaweb/audio_photo">Audio Photo/video</a>
                 <ul class="menu vertical">
                     <li><a href="test.html">asdf</a></li>
                     <li><a href="test.html">asdf</a></li>
                 </ul>
             </li>
-            <li><a href="informations.html">Informations</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="/paginaweb/informations">Informations</a></li>
+            <li><a href="/paginaweb/contact">Contact</a></li>
         </ul>
     </div>
 </div>
 
-<script src="js/vendor/jquery.js"></script>
-<script src="js/vendor/foundation.js"></script>
+<script src="/paginaweb/js/vendor/jquery.js"></script>
+<script src="/paginaweb/js/vendor/foundation.js"></script>
 <script>
     $(document).foundation();
     var elem = new Foundation.DropdownMenu(".dropdown");
