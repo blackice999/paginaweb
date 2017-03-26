f6a409919668fde098641fd3fc3defa490d7cdff
blackice999 <adam596341@yahoo.com>
2017-03-26 17:58:17 +0300

    Add webpage menu

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
     <link rel="stylesheet" type="text/css" href="css/foundation.css">
 </head>
 <body>
-    <div class="row">
-        <div class="medium-8 medium-centered columns">
-            <img class="float-center" src="img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
-        </div>
-    </div>
+    <?php include "menu.php"; ?>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,54 @@
+<div class="row">
+
+    <div class="large-10 medium-10 small-10 small-centered medium-centered large-centered columns">
+        <img class="float-center" src="img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
+
+        <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+            <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+            <div class="title-bar-title">Menu</div>
+        </div>
+
+        <ul class="dropdown menu" data-dropdown-menu id="example-menu">
+            <li><a href="index.php" class="active">Home</a></li>
+            <li><a href="computer_parts.html">Computer parts</a>
+                <ul class="menu vertical">
+                    <li><a href="motherboards.html">Motherboards</a></li>
+                    <li><a href="motherboards.html">Motherboards</a></li>
+                </ul>
+            </li>
+            <li><a href="peripherials.html">Peripherials</a>
+                <ul class="menu vertical">
+                    <li><a href="test.html">asdf</a></li>
+                    <li><a href="test.html">asdf</a></li>
+                </ul>
+            </li>
+            <li><a href="software.html">Software</a>
+                <ul class="menu vertical">
+                    <li><a href="test.html">asdf</a></li>
+                    <li><a href="test.html">asdf</a></li>
+                </ul>
+            </li>
+            <li><a href="telephones.html">Telephones</a>
+                <ul class="menu vertical">
+                    <li><a href="test.html">asdf</a></li>
+                    <li><a href="test.html">asdf</a></li>
+                </ul>
+            </li>
+            <li><a href="audio_foto">Audio Photo/video</a>
+                <ul class="menu vertical">
+                    <li><a href="test.html">asdf</a></li>
+                    <li><a href="test.html">asdf</a></li>
+                </ul>
+            </li>
+            <li><a href="informations.html">Informations</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </div>
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script>
+    $(document).foundation();
+    var elem = new Foundation.DropdownMenu(".dropdown");
+</script>
