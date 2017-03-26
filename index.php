3757fcae383f29a97c46fd5f5e3a20948f7e9e67
blackice999 <adam596341@yahoo.com>
2017-03-26 16:42:20 +0300

    Add banner image

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Adam
- * Date: 26.03.2017
- * Time: 16:24
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web shop</title>
+    <link rel="stylesheet" type="text/css" href="css/foundation.css">
+</head>
+<body>
+    <div class="row">
+        <div class="medium-8 medium-centered columns">
+            <img class="float-center" src="img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
+        </div>
+    </div>
+</body>
+</html>
