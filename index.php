9eb35df3587f24a619e565683aa672b4517cde77
blackice999 <adam596341@yahoo.com>
2017-04-02 16:26:13 +0300

    Add rootPath to links with PHP

--- index.php
+++ index.php
@@ -5,6 +5,27 @@
     <title>Web shop</title>
     <link rel="stylesheet" type="text/css" href="/paginaweb/css/foundation.css">
     <link rel="stylesheet" type="text/css" href="/paginaweb/css/app.css">
+
+    <style>
+
+    </style>
+
+    <script>
+
+        var jsonPath = window.location.pathname;
+
+        components = jsonPath.split("/");
+
+        rootPath = components[1];
+
+        //Remove root and /paginaweb from the path
+        components.shift();
+        components.shift();
+
+//        document.write(components);
+        var path = components.join("/");
+        document.write(path);
+    </script>
 </head>
 
 <body>
@@ -20,5 +41,22 @@
         }
 
     ?>
+
+<script>
+    var arr = [], links = document.links;
+
+    for(var i = 0; i< links.length; i++){
+        arr.push(links[i].pathname);
+    }
+
+    console.log(arr);
+
+    element = arr[10];
+//    element.shift();
+    console.log(element);
+
+    var matches = document.querySelectorAll("[href='/paginaweb/monitors']");
+    console.log(matches);
+    </script>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,8 +1,9 @@
+<?php $rootPath = "/paginaweb/"?>
 <div class="row">
 
     <div class="large-10 medium-10 small-10 small-centered medium-centered large-centered columns">
-        <a href="/paginaweb/index">
-            <img class="float-center" src="/paginaweb/img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
+        <a href="<?php echo $rootPath; ?>index">
+            <img class="float-center" src="<?php echo $rootPath; ?>img/workstation-147953_960_720.png" style="width: 30%; height: 30%;">
         </a>
 
         <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
@@ -11,73 +12,73 @@
         </div>
 
         <ul class="dropdown menu" data-dropdown-menu id="example-menu">
-            <li><a href="/paginaweb/computer_parts">Computer parts</a>
+            <li><a href="<?php echo $rootPath; ?>computer_parts">Computer parts</a>
                 <ul class="menu vertical">
-                    <li><a href="motherboards">Motherboards</a></li>
-                    <li><a href="video_cards">Video cards</a></li>
-                    <li><a href="processors">Processors</a></li>
-                    <li><a href="ssds">SSDs</a></li>
-                    <li><a href="hdds">HDDs</a></li>
-                    <li><a href="power_supplies">Power supplies</a></li>
-                    <li><a href="chassis">Chassis</a></li>
+                    <li><a href="<?php echo $rootPath; ?>motherboards">Motherboards</a></li>
+                    <li><a href="<?php echo $rootPath; ?>video_cards">Video cards</a></li>
+                    <li><a href="<?php echo $rootPath; ?>processors">Processors</a></li>
+                    <li><a href="<?php echo $rootPath; ?>ssds">SSDs</a></li>
+                    <li><a href="<?php echo $rootPath; ?>hdds">HDDs</a></li>
+                    <li><a href="<?php echo $rootPath; ?>power_supplies">Power supplies</a></li>
+                    <li><a href="<?php echo $rootPath; ?>chassis">Chassis</a></li>
                 </ul>
             </li>
-            <li><a href="/paginaweb/peripherials">Peripherials</a>
+            <li><a href="<?php echo $rootPath; ?>peripherials">Peripherials</a>
                 <ul class="menu vertical">
-                    <li><a href="monitors">Monitors</a></li>
-                    <li><a href="mice">Mice</a></li>
-                    <li><a href="keyboards">Keyboardss</a></li>
-                    <li><a href="external_hdds">External HDDs</a></li>
+                    <li><a href="<?php echo $rootPath; ?>monitors">Monitors</a></li>
+                    <li><a href="<?php echo $rootPath; ?>mice">Mice</a></li>
+                    <li><a href="<?php echo $rootPath; ?>keyboards">Keyboardss</a></li>
+                    <li><a href="<?php echo $rootPath; ?>external_hdds">External HDDs</a></li>
                 </ul>
             </li>
-            <li><a href="/paginaweb/software">Software</a>
+            <li><a href="<?php echo $rootPath; ?>software">Software</a>
                 <ul class="menu vertical">
-                    <li><a href="operating_system">Operating systems</a></li>
-                    <li><a href="office_apps">Office applications</a></li>
-                    <li><a href="security_solutions">Security solutions</a></li>
+                    <li><a href="<?php echo $rootPath; ?>operating_system">Operating systems</a></li>
+                    <li><a href="<?php echo $rootPath; ?>office_apps">Office applications</a></li>
+                    <li><a href="<?php echo $rootPath; ?>security_solutions">Security solutions</a></li>
                 </ul>
             </li>
-            <li><a href="/paginaweb/telephones">Telephones</a>
+            <li><a href="<?php echo $rootPath; ?>telephones">Telephones</a>
                 <ul class="menu vertical">
-                    <li><a href="smartphones">Smartphones</a></li>
-                    <li><a href="smartwatches">Smartwatches</a></li>
-                    <li><a href="external_batteries"> External batteries</a></li>
-                    <li><a href="/paginaweb/gsm_accessories">GSM accesories</a>
+                    <li><a href="<?php echo $rootPath; ?>smartphones">Smartphones</a></li>
+                    <li><a href="<?php echo $rootPath; ?>smartwatches">Smartwatches</a></li>
+                    <li><a href="<?php echo $rootPath; ?>external_batteries"> External batteries</a></li>
+                    <li><a href="<?php echo $rootPath; ?>gsm_accessories">GSM accesories</a>
                         <ul class="menu vertical">
-                            <li><a href="/paginaweb/gsm_accessories/selfie_sticks">Selfie sticks</a></li>
-                            <li><a href="/paginaweb/gsm_accessories/memory_cards">Memory cards</a></li>
-                            <li><a href="/paginaweb/gsm_accessories/chargers">Chargers</a></li>
-                            <li><a href="/paginaweb/gsm_accessories/wireless_chargers">Wireless chargers</a></li>
+                            <li><a href="<?php echo $rootPath; ?>gsm_accessories/selfie_sticks">Selfie sticks</a></li>
+                            <li><a href="<?php echo $rootPath; ?>gsm_accessories/memory_cards">Memory cards</a></li>
+                            <li><a href="<?php echo $rootPath; ?>gsm_accessories/chargers">Chargers</a></li>
+                            <li><a href="<?php echo $rootPath; ?>gsm_accessories/wireless_chargers">Wireless chargers</a></li>
                         </ul>
                     </li>
                 </ul>
             </li>
-            <li><a href="/paginaweb/audio_photo">Audio Photo/video</a>
+            <li><a href="<?php echo $rootPath; ?>audio_photo">Audio Photo/video</a>
                 <ul class="menu vertical">
-                    <li><a href="/paginaweb/speakers">Speakers</a>
+                    <li><a href="<?php echo $rootPath; ?>speakers">Speakers</a>
                         <ul class="menu vertical">
-                            <li><a href="/paginaweb/speakers/portable_speakers">Portable speakers</a></li>
+                            <li><a href="<?php echo $rootPath; ?>speakers/portable_speakers">Portable speakers</a></li>
                         </ul>
                     </li>
-                    <li><a href="microphones">Microphones</a></li>
-                    <li><a href="microphones">Cameras</a>
+                    <li><a href="<?php echo $rootPath; ?>microphones">Microphones</a></li>
+                    <li><a href="<?php echo $rootPath; ?>microphones">Cameras</a>
                          <ul class="menu vertical">
-                            <li><a href="/paginaweb/cameras/d_slr">D-SLR Cameras</a></li>
-                            <li><a href="/paginaweb/cameras/compact">Compact Cameras</a></li>
-                            <li><a href="/paginaweb/cameras/bridge">Bridge Cameras</a></li>
+                            <li><a href="<?php echo $rootPath; ?>cameras/d_slr">D-SLR Cameras</a></li>
+                            <li><a href="<?php echo $rootPath; ?>cameras/compact">Compact Cameras</a></li>
+                            <li><a href="<?php echo $rootPath; ?>cameras/bridge">Bridge Cameras</a></li>
                         </ul>
                     </li>
                 </ul>
             </li>
-            <li><a href="/paginaweb/informations">Informations</a></li>
-            <li><a href="/paginaweb/contact">Contact</a></li>
-            <li><a href="/paginaweb/account">My account</a></li>
+            <li><a href="<?php echo $rootPath; ?>informations">Informations</a></li>
+            <li><a href="<?php echo $rootPath; ?>contact">Contact</a></li>
+            <li><a href="<?php echo $rootPath; ?>account">My account</a></li>
         </ul>
     </div>
 </div>
 
-<script src="/paginaweb/js/vendor/jquery.js"></script>
-<script src="/paginaweb/js/vendor/foundation.js"></script>
+<script src="<?php echo $rootPath; ?>js/vendor/jquery.js"></script>
+<script src="<?php echo $rootPath; ?>js/vendor/foundation.js"></script>
 <script>
     $(document).foundation();
     var elem = new Foundation.DropdownMenu(".dropdown");
