c87ace66e812cc20f2d9eee82e5365f45ed74910
blackice999 <adam596341@yahoo.com>
2017-04-02 15:01:00 +0300

    Change menu items color and arrow color to black to look better when .active state is added

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <title>Web shop</title>
     <link rel="stylesheet" type="text/css" href="/paginaweb/css/foundation.css">
+    <link rel="stylesheet" type="text/css" href="/paginaweb/css/app.css">
 </head>
+
 <body>
-    <?php include "menu.php"; ?>
+    <?php
+
+        include "menu.php";
+
+        $path = $_GET['path'];
+        echo $path;
+
+        if($path === "software") {
+            echo "in software";
+        }
+
+    ?>
 </body>
 </html>
