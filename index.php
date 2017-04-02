04cbc9a8a4508a606c17ac6867d0bc1d0f7545ae
blackice999 <adam596341@yahoo.com>
2017-04-02 20:02:27 +0300

    Add register form

--- index.php
+++ index.php
@@ -19,7 +19,59 @@
             echo "in software";
         }
 
+        if($path === "account"):
+            //If not logged in, show login form
     ?>
+        <form method="post" action="login.php" style="margin-top: 20px;">
+            <div class="row">
+                <div class="large-10 columns">
+                    <label>
+                        Username
+                        <input type="text" name="username">
+                    </label>
+
+                    <label>
+                        Password
+                        <input type="password" name="password">
+                    </label>
+                    <input type="submit" class="button" value="Log in">
+                </div>
+
+            </div>
+        </form>
+    <?php
+        //else show register form
+//        else:
+    ?>
+        <div class="row">
+            <div class="large-10 columns">
+                <h2> Register </h2>
+                <form method="post" action="register.php">
+                    <label>
+                        Full name
+                        <input type="text" name="name">
+                    </label>
+                    <label>
+                        Address
+                        <input type="text" name="address">
+                    </label>
+                    <label>
+                        Username
+                        <input type="text" name="username">
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" name="password">
+                    </label>
+                    <label>
+                        Repeat password
+                        <input type="password" name="repeat_password">
+                    </label>
+                    <input type="submit" class="button" value="Register">
+                 </form>
+            </div>
+    </div>
+        <?php endif; ?>
 
 <script>
     path = window.location.pathname;
