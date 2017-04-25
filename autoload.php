33306b137e1751e37c41d5ac920f0ce4abed292d
blackice999 <adam596341@yahoo.com>
2017-04-25 19:25:05 +0300

    Load config file

--- autoload.php
+++ autoload.php
@@ -14,3 +14,4 @@ if (!file_exists(BASE_PATH . '/vendor/autoload.php')) {
     die('Please run `composer install` to generate your vendor directory');
 }
 require_once BASE_PATH . '/vendor/autoload.php';
+require_once "config.php";
