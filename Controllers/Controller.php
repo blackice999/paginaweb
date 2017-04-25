987db372d4ee17875eb70e16ca3ce72921402ec9
blackice999 <adam596341@yahoo.com>
2017-04-25 19:24:35 +0300

    Add mandatory method types to have in controllers

---
+++ Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 21.04.2017
+ * Time: 19:09
+ */
+
+namespace Controllers;
+
+
+interface Controller
+{
+    public function get();
+    public function post();
+}
