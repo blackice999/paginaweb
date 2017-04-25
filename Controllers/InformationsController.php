50cb8641e9d5a0965ed477565553cc8587ede66c
blackice999 <adam596341@yahoo.com>
2017-04-25 17:29:48 +0300

    Generate menu from array

--- Controllers/InformationsController.php
+++ Controllers/InformationsController.php
@@ -11,6 +11,54 @@ namespace Controllers;
 
 class InformationsController implements Controller
 {
+
+    private static $menu = [
+        "contact" => [
+            "Contact us" => [
+                "contact" => "Contact"
+            ]
+        ],
+        "orders" => [
+            "Orders" => [
+                "how_to_order" => "How to order",
+                "shipping_term" => "Shipping term",
+                "terms" => "Terms and conditions",
+                "order_state" => "Order state",
+                "client_advantages" => "Client advantages",
+                "order_cancellation" => "Order cancellation",
+                "product_availability" => "Product availability"
+            ]
+        ],
+        "returns_warranty" => [
+            "Returns and warranty" => [
+                "return_policy" => "Return policy",
+                "warranty_conditions" => "Warranty conditions",
+                "how_to_proceed" => "How do I proceed"
+            ]
+        ],
+        "payment_methods" => [
+            "Payment methods" => [
+                "repayment" => "Repayment",
+                "with_card" => "With card",
+                "cash_hq" => "Cash at the HQ",
+                "rates" => "Rates"
+            ]
+        ],
+        "delivery" => [
+            "Delivery" => [
+                "costs" => "Costs",
+                "methods" => "Methods",
+                "urgent_delivery" => "Urgent delivery in Baia Mare in just 3 hours"
+            ]
+        ],
+        "about_us" => [
+            "About us" => [
+                "company" => "Company",
+                "products" => "Products"
+            ]
+        ]
+    ];
+
     public function post()
     {
         if (isset($_POST['login'])) {
@@ -25,54 +73,21 @@ class InformationsController implements Controller
 
     private function showMenu()
     {
-        echo "
-            <div class='large-2 medium-3 small-3 columns'>
-                <ul class='vertical menu'>
-                    <li><a href='contact' style='background-color: #D0D0D0;'>Contact us</a></li>
-                    <ul class='nested vertical menu'>
-                        <li><a href='contact'>Contact</a></li>
-                    </ul>
+        echo "<div class='large-2 medium-3 small-3 columns'>";
+        echo "<ul class='vertical menu'>";
+        foreach (self::$menu as $mainLinks => $mainTitles) {
+            foreach ($mainTitles as $title => $subLinks) {
+                echo "<li><a href=$mainLinks style='background-color: #D0D0D0;'>$title</a></li>";
+                foreach ($subLinks as $link => $value) {
+                    echo "<ul class='nested vertical menu'>
+                             <li><a href=$link>$value</a></li>
+                        </ul>";
 
-                   <li><a href='orders' style='background-color: #D0D0D0;'>Orders</a></li>
-                   <ul class='nested vertical menu'>
-                        <li><a href='how_to_order'>How to order</a></li>
-                        <li><a href='shipping_term'>Shipping term</a></li>
-                        <li><a href='terms'>Terms and conditions</a></li>
-                        <li><a href='order_state'>Order state</a></li>
-                        <li><a href='client_advantages'>Client advantages</a></li>
-                        <li><a href='order_cancelation'>Order cancelation</a></li>
-                        <li><a href='product_availability'>Product availability</a></li>
-                   </ul>
+                }
+            }
+        }
 
-                   <li><a href='returns_warranty' style='background-color: #D0D0D0;'>Returns and warranty</a></li>
-                   <ul class='nested vertical menu'>
-                        <li><a href='return_policy'>Return policy</a></li>
-                        <li><a href='warranty_conditions'>Warranty conditions</a></li>
-                        <li><a href='how_to_proceed'>How do I proceed</a></li>
-                   </ul>
-
-                   <li><a href='payment_methods' style='background-color: #D0D0D0;'>Payment methods</a></li>
-                   <ul class='nested vertical menu'>
-                        <li><a href='repayment'>Repayment</a></li>
-                        <li><a href='with_card'>With card</a></li>
-                        <li><a href='cash_hq'>Cash at the HQ</a></li>
-                        <li><a href='rates'>Rates</a></li>
-                   </ul>
-
-                   <li><a href='delivery' style='background-color: #D0D0D0;'>Delivery</a></li>
-                   <ul class='nested vertical menu'>
-                        <li><a href='costs'>Costs</a></li>
-                        <li><a href='methods'>Methods</a></li>
-                        <li><a href='urgent_delivery'>Urgent delivery in Baia Mare in just 3 hours</a></li>
-                   </ul>
-
-                   <li><a href='about_us' style='background-color: #D0D0D0;'>About us</a></li>
-                   <ul class='nested vertical menu'>
-                        <li><a href='company'>Company</a></li>
-                        <li><a href='products'>Products</a></li>
-                   </ul>
-                </ul>
-          </div>
-        ";
+        echo "</ul>";
+        echo "</div>";
     }
 }
