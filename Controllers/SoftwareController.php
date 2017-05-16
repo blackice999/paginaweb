bdab380d3266eb17660f264364e4575da8eb920f
blackice999 <adam596341@yahoo.com>
2017-05-16 19:35:28 +0300

    Display subcategories using magic method

--- Controllers/SoftwareController.php
+++ Controllers/SoftwareController.php
@@ -9,12 +9,17 @@
 namespace Controllers;
 
 
+use Models\ProductModel;
+use Models\ProductSpecModel;
 use Utils\HTMLGenerator;
 use Utils\StringUtils;
 
 class SoftwareController implements Controller
 {
     private $categories = ["operating_systems", "office_apps", "security_solutions"];
+    const OPERATING_SYSTEMS_CATEGORY_ID = 15;
+    const OFFICE_APPS_CATEGORY_ID = 16;
+    const SECURITY_SOLUTIONS_CATEGORY_ID = 17;
 
     public function get()
     {
@@ -31,11 +36,48 @@ class SoftwareController implements Controller
 
     public function post()
     {
-        // TODO: Implement post() method.
+
     }
 
     public function __call($name, $arguments)
     {
-        echo $name;
+        $categoryId = constant("self::" . strtoupper($arguments[0]) . "_CATEGORY_ID");
+
+        echo "<div class=\"row small-up-2 medium-up-5 large-up-3\" style='margin-top:10px;'>";
+
+        if (empty(ProductModel::loadByCategoryId($categoryId))) {
+            HTMLGenerator::tag("h2", "No " . StringUtils::removeUnderscore($name) . " found");
+        } else {
+
+            foreach (ProductModel::loadByCategoryId($categoryId) as $category) {
+                echo "<div class=\"column\" style='border: 1px solid black; padding: 10px; height: 400px;'>";
+                HTMLGenerator::image("//placehold.it/150x150", "placeholder 150x150",
+                    "float-center", "margin-bottom:30px");
+                HTMLGenerator::link("motherboards/" . $category->id, $category->name,
+                    "float-center text-center", "margin-bottom:30px");
+
+                echo " <ul>";
+                foreach ($category->getProductSpecModel() as $productSpecModel) {
+                    echo "<li>" . $productSpecModel->name . "</li>";
+                }
+                echo "</ul>";
+
+                HTMLGenerator::tag("h3", "$" . $category->price);
+                echo "</div>";
+            }
+
+            echo "</div>";
+        }
+
+        HTMLGenerator::row(5, 5, 5);
+        HTMLGenerator::tag("h2", "Add a new " . StringUtils::removeUnderscore(StringUtils::toSingular($name)));
+        HTMLGenerator::form("post", $_GET['path'], [
+            ["label" => "Name", "type" => "text", "name" => "name", "value" => ""],
+            ["label" => "Description", "type" => "text", "name" => "description", "value" => ""],
+            ["label" => "price", "type" => "text", "name" => "price", "value" => ""],
+            ["label" => "Specification name", "type" => "text", "name" => "spec_name", "value" => ""],
+            ["label" => "", "type" => "submit", "name" => "submit", "value" => "Insert motherboard"]
+        ]);
+        HTMLGenerator::closeRow();
     }
 }
