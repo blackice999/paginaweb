ca18cffca7421bf0867c18890b8f9e30031a1a6e
blackice999 <adam596341@yahoo.com>
2017-05-16 19:35:50 +0300

    Insert a new software in database

--- Controllers/SoftwareController.php
+++ Controllers/SoftwareController.php
@@ -36,7 +36,17 @@ class SoftwareController implements Controller
 
     public function post()
     {
+        if (isset($_POST['submit'])) {
+            $categoryId = constant("self::" . strtoupper($_GET['path']) . "_CATEGORY_ID");
+            $name = StringUtils::sanitizeString($_POST['name']);
+            $description = StringUtils::sanitizeString($_POST['description']);
+            $price = StringUtils::sanitizeString($_POST['price']);
+            $specName = StringUtils::sanitizeString($_POST['spec_name']);
 
+            $result = ProductModel::create($categoryId, "products", $name, $description, $price);
+            HTMLGenerator::tag("p", "Inserted new " . StringUtils::removeUnderscore(StringUtils::toSingular($_GET['path'])) . " with the id " . $result->id);
+            ProductSpecModel::create($result->id, $specName);
+        }
     }
 
     public function __call($name, $arguments)
