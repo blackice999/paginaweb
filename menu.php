7ce90d7ba2893193f2ba5b0fbdf06e1fb776a59f
blackice999 <adam596341@yahoo.com>
2017-05-17 17:33:26 +0300

    Use class to generate links

--- menu.php
+++ menu.php
@@ -40,32 +40,28 @@ $test = [
 ];
 
 echo " <div class=\"large-10 medium-10 small-10 small-centered medium-centered large-centered columns\">";
-\Utils\HTMLGenerator::link($rootPath . "index",
+echo \Utils\HTMLGenerator::link($rootPath . "index",
     \Utils\HTMLGenerator::image($rootPath . "img/workstation-147953_960_720.png", "", "float-center", "width: 30%; height: 30%;"));
 echo " <ul class=\"dropdown menu\" data-dropdown-menu id=\"example-menu\">";
 foreach ($menu as $menuItem => $item) {
     echo "<li>" . \Utils\HTMLGenerator::link($rootPath . $menuItem, ucfirst(\Utils\StringUtils::removeUnderscore($menuItem)));
-//    echo "<li><a href='" . $rootPath . $menuItem . "'>" . ucfirst(\Utils\StringUtils::removeUnderscore($menuItem)) . "</a>";
     foreach ($item as $key => $value) {
         if (!empty($value)) {
             echo "<ul class='menu vertical'>";
             foreach ($value as $subcategory => $subsub) {
                 if (is_array($subsub)) {
                     //Displays the key for the subsubcategories, like GSm accssories
-//                echo "<li>" . \Utils\HTMLGenerator::link($rootPath . $subcategory, ucfirst(\Utils\StringUtils::removeUnderscore($subcategory)));
-                    echo "<li><a href='" . $rootPath . $subcategory . "'>" . ucfirst(\Utils\StringUtils::removeUnderscore($subcategory)) . "</a>";
+                    echo "<li>" . \Utils\HTMLGenerator::link($rootPath . $subcategory, ucfirst(\Utils\StringUtils::removeUnderscore($subcategory)));
                     foreach ($subsub as $subsubsub => $keys) {
                         echo "<ul class='menu vertical'>";
                         foreach ($keys as $item) {
-//                        echo "<li>" . \Utils\HTMLGenerator::link($rootPath . $item, ucfirst(\Utils\StringUtils::removeUnderscore($item))) . "</li>";
-                            echo "<li><a href='" . $rootPath . $item . "'>" . ucfirst(\Utils\StringUtils::removeUnderscore($item)) . "</a></li>";
+                            echo "<li>" . \Utils\HTMLGenerator::link($rootPath . $item, ucfirst(\Utils\StringUtils::removeUnderscore($item))) . "</li>";
                         }
                         echo "</ul>";
                     }
                     echo "</li>";
                 } else {
-//                echo "<li> " . \Utils\HTMLGenerator::link($rootPath . $subsub, ucfirst(\Utils\StringUtils::removeUnderscore($subsub))) . "</li>";
-                    echo "<li><a href='" . $rootPath . $subsub . "'>" . ucfirst(\Utils\StringUtils::removeUnderscore($subsub)) . "</a></li>";
+                    echo "<li> " . \Utils\HTMLGenerator::link($rootPath . $subsub, ucfirst(\Utils\StringUtils::removeUnderscore($subsub))) . "</li>";
                 }
             }
             echo "</ul>";
