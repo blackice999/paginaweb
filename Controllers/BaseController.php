c55ff177e85fcef3866dba2e825faeb54b19dc96
blackice999 <adam596341@yahoo.com>
2017-10-27 19:33:27 +0300

    Display only 5 products when viewing all subcategories

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -19,6 +19,50 @@ use Utils\StringUtils;
 abstract class BaseController implements Controller
 {
 
+    //Computer parts
+    const MOTHERBOARDS_CATEGORY_ID = 2;
+    const VIDEO_CARDS_CATEGORY_ID = 3;
+    const PROCESSORS_CATEGORY_ID = 4;
+    const SSDS_CATEGORY_ID = 5;
+    const HDDS_CATEGORY_ID = 6;
+    const POWER_SUPPLIES_CATEGORY_ID = 7;
+    const CHASSIS_CATEGORY_ID = 8;
+
+    //Peripherials
+    const MONITORS_CATEGORY_ID = 10;
+    const MICE_CATEGORY_ID = 11;
+    const KEYBOARDS_CATEGORY_ID = 12;
+    const EXTERNAL_HDDS_CATEGORY_ID = 13;
+
+    //Software
+    const OPERATING_SYSTEMS_CATEGORY_ID = 15;
+    const OFFICE_APPS_CATEGORY_ID = 16;
+    const SECURITY_SOLUTIONS_CATEGORY_ID = 17;
+
+    //Telephones
+    const SMARTPHONES_CATEGORY_ID = 19;
+    const SMARTWATCHES_CATEGORY_ID = 20;
+    const EXTERNAL_BATTERIES_CATEGORY_ID = 21;
+
+    //GSM accessories
+    const SELFIE_STICKS_CATEGORY_ID = 23;
+    const MEMORY_CARDS_CATEGORY_ID = 24;
+    const CHARGERS_CATEGORY_ID = 25;
+    const WIRELESS_CHARGERS_CATEGORY_ID = 26;
+
+    //Audio photo-video
+    const SPEAKERS_CATEGORY_ID = 28;
+    const MICROPHONES_CATEGORY_ID = 29;
+    const CAMERAS_CATEGORY_ID = 30;
+
+    //Speakers
+    const PORTABLE_SPEAKERS_CATEGORY_ID = 31;
+
+    //Cameras
+    const D_SLRS_CATEGORY_ID = 32;
+    const COMPACTS_CATEGORY_ID = 33;
+    const BRIDGES_CATEGORY_ID = 34;
+
     public function get()
     {
         // TODO: Implement get() method.
@@ -36,18 +80,27 @@ abstract class BaseController implements Controller
         } else {
             foreach ($categories as $category) {
                 $titleFromLink = StringUtils::removeUnderscore($category);
-                echo HTMLGenerator::tag("h3", ucfirst($titleFromLink));
-                echo HTMLGenerator::link($category, "Check all " . $titleFromLink);
+
+                echo HTMLGenerator::tag("h3", ucfirst($titleFromLink)
+                    . HTMLGenerator::link($category, "Check all " . $titleFromLink,
+                        "", "font-size:14px; margin-left:5px;"
+                    )
+                );
+                $categoryId = constant("self::" . strtoupper($category) . "_CATEGORY_ID");
+                echo "<div class=\"row small-up-2 medium-up-5 large-up-3\" style='margin-top:10px;'>";
+                $this->displayProducts($categoryId, $category, false);
+                HTMLGenerator::closeRow();
             }
         }
     }
 
-    public function displayProducts(int $categoryId, string $categoryName)
+    public function displayProducts(int $categoryId, string $categoryName, bool $showAll = true)
     {
         if (empty(ProductModel::loadByCategoryId($categoryId))) {
             echo HTMLGenerator::tag("h2", "No " . StringUtils::removeUnderscore($categoryName) . " found");
         } else {
 
+            $i = 0;
             foreach (ProductModel::loadByCategoryId($categoryId) as $product) {
                 echo "<div class=\"column\" style='border: 1px solid black; padding: 10px; height: 400px;'>";
 
@@ -92,6 +145,14 @@ abstract class BaseController implements Controller
 
                 $this->productActionsForm($product);
                 echo "</div>";
+
+
+                if (!$showAll) {
+                    $i++;
+                    if ($i > 5) {
+                        break;
+                    }
+                }
             }
 
             echo "</div>";
